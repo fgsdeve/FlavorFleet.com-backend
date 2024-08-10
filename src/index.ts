9473d1b269c4c4c0562f961a95c2b38d3b52c78e
Francdeve <156461955+fgsdeve@users.noreply.github.com>
2024-08-10 15:42:17 -0500

    read to deploy

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,27 @@ import mongoose from "mongoose";
 import MyUserRoute from './routes/MyUserRoute';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
-.then(() => console.log('Connected to the database!'))
-.catch((err) => console.error('Database connection error:', err));
+  .then(() => console.log('Connected to the database!'))
+  .catch((err) => console.error('Database connection error:', err));
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
+// Configure CORS
+const corsOptions = {
+  origin: 'https://flavorfleet-com-frontend.onrender.com', // Allow requests from your frontend
+  optionsSuccessStatus: 200 // For legacy browser support
+};
+
+app.use(cors(corsOptions));
 
 app.get("/health", async (req: Request, res: Response) => {
-    res.send({ message: "health OK!" });
-  });
+  res.send({ message: "health OK!" });
+});
 
-app.use("/api/my/user", MyUserRoute)
+app.use("/api/my/user", MyUserRoute);
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
-    console.log(`Server started on localhost:${PORT}`);
+  console.log(`Server started on localhost:${PORT}`);
 });
